da88840d6f176284f96aa65d493b38006f982dc4
rkfg <rkfg@rkfg.me>
2021-08-01 16:55:34 +0300

    Exclude components with my own pubkey

--- components.go
+++ components.go
@@ -5,11 +5,17 @@ import (
 	"gonum.org/v1/gonum/graph/traverse"
 )
 
-func findComponents(g graph.Graph, n graph.Node, min int) [][]graph.Node {
+func findComponents(g graph.Graph, n graph.Node, pubkey string, min int) [][]graph.Node {
 	result := [][]graph.Node{}
 	compnodes := []graph.Node{}
 	visited := map[graph.Node]struct{}{}
+	pubkeyFound := false
 	w := traverse.DepthFirst{Visit: func(n graph.Node) {
+		if nn, ok := n.(Node); ok {
+			if nn.Pubkey == pubkey {
+				pubkeyFound = true
+			}
+		}
 		compnodes = append(compnodes, n)
 		visited[n] = struct{}{}
 	}, Traverse: func(e graph.Edge) bool {
@@ -20,8 +26,9 @@ func findComponents(g graph.Graph, n graph.Node, min int) [][]graph.Node {
 		if _, ok := visited[c]; !ok {
 			w.Reset()
 			compnodes = []graph.Node{}
+			pubkeyFound = false
 			w.Walk(g, c, nil)
-			if len(compnodes) > min {
+			if len(compnodes) > min && !pubkeyFound {
 				result = append(result, compnodes)
 			}
 		}
